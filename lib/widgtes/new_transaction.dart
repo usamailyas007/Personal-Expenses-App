5be2a9bdb9a4798201582cf7635a2f21c9c81cc8
usamailyas007 <usil7d@gmail.com>
2023-04-06 13:55:49 -0700

    initial commit
    App Completed...!

--- lib/widgtes/new_transaction.dart
+++ lib/widgtes/new_transaction.dart
@@ -55,7 +55,12 @@ class _newTransactionsState extends State<newTransactions> {
     return Card(
         elevation: 5,
         child: Container(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
